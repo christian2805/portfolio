f79dda8c488600df47722150d143e4866db92765
Chriss <=>
2019-10-27 19:16:48 +0100

    glemte at den var på git --første udgave--

--- script.js
+++ script.js
@@ -3,29 +3,45 @@ document.addEventListener("DOMContentLoaded", start);
 function start() {}
 const videos = document.querySelectorAll(".videoz");
 document.getElementById("burger").addEventListener("click", openSidebar);
+const coll = document.getElementsByClassName("readMore");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 function openSidebar() {
   if (!(burgermenu.style.display == "block")) {
     burgermenu.style.display = "block";
+    burgermenu.classList.remove("menu_tilbage");
+    burgermenu.classList.add("menu_frem");
   } else {
-    burgermenu.style.display = "none";
+    burgermenu.classList.remove("menu_frem");
+    burgermenu.classList.add("menu_tilbage");
+    setTimeout(function() {
+      burgermenu.style.display = "none";
+    }, 500);
   }
 }
 videos.forEach(video => {
   video.addEventListener("mouseover", function() {
-    console.log("play");
     this.play();
   });
   video.addEventListener("mouseout", function() {
-    console.log("pause");
     this.pause();
   });
   video.addEventListener("touchstart", function() {
-    console.log("touchplay");
     this.play();
   });
   video.addEventListener("touchend", function() {
-    console.log("touchpause");
     this.play();
   });
 });
